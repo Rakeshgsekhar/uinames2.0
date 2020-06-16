cce150b6be69342273011b40f7bc554704dad1fb
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2020-06-16 19:34:49 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    main: "./index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "uinames.min.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+};
