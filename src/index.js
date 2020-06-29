18602e9b535ab0cfa82113d91cfd254b5028108d
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2020-06-29 20:17:45 +0530

    removed comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-// const { signedOut, signUp } = require("../controllers/auth");
 const { getNames, getRegions } = require("./controller/uinameController");
 
 router.post("/getnames", getNames);
 router.get("/getregions", getRegions);
-
-//router.post("/signup", signUp);
 
 module.exports = router;
